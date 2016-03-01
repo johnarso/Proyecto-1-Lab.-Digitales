5ebbff29b3256ddd97e703a63c4086f86a574ec8
Mels075 <mfonsecarodriguez83@gmail.com>
2016-03-01 16:13:13 -0600

    Memoria para display
    Cambios realizados

--- Memoria_display.v
+++ Memoria_display.v
@@ -21,28 +21,31 @@
 module Memoria_display(
     input CLK,
 	 input [3:0] numero, 
+	 input reset,
 	 output [7:0] controles_display
 	 );
 	 
-	 reg lolo;
 	 reg [7:0] codigo;
 	 
 	 always @(posedge CLK)
-		begin
+	 begin
+		 if (reset)
+			codigo<=3;
+		 else
 			case(numero)
-				4'b0000: codigo=3;
-				4'b0001: codigo=159;
-				4'b0010: codigo=37;
-				4'b0011: codigo=13;
-				4'b0100: codigo=153;
-				4'b0101: codigo=73;
-				4'b0110: codigo=65;
-				4'b0111: codigo=31;
-				4'b1000: codigo=1;
-				4'b1001: codigo=9;
-				default: lolo=1;
+				4'b0000: codigo<=3;
+				4'b0001: codigo<=159;
+				4'b0010: codigo<=37;
+				4'b0011: codigo<=13;
+				4'b0100: codigo<=153;
+				4'b0101: codigo<=73;
+				4'b0110: codigo<=65;
+				4'b0111: codigo<=31;
+				4'b1000: codigo<=1;
+				4'b1001: codigo<=9;
+				default: codigo<=3;
 			endcase
-		end
+	 end
 	
 	assign controles_display=codigo;
 
