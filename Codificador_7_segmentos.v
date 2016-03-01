2693ae05927bde12511603b611daf8d989c99b53
Mels075 <mfonsecarodriguez83@gmail.com>
2016-03-01 12:26:31 -0600

    Codificador 7 segmentos
    en proceso de simulacion

--- Codificador_7_segmentos.v
+++ Codificador_7_segmentos.v
@@ -23,29 +23,52 @@ module Codificador_7_segmentos(
 	 input [7:0] frecuencia,
 	 input control,
 	 input [9:0] corriente,
-	 output [3:0] selec_digito,
-	 output [7:0] numero_cod
+	 output reg [3:0] selec_digito,
+	 output reg [7:0] numero_cod
 	 );
-	 
-	 reg unidades,decenas,centenas;
-	 reg auxiliar;
-	 
+	 reg [9:0]var;
+	 reg [3:0]unidades;
+	 reg [3:0]decenas;
+	 reg [3:0]centenas;
+	 reg [3:0]millares;
+	 reg [7:0]auxiliar;
+	 reg [7:0]auxiliar2;
+	 reg [3:0]num;
+	
+	always @(posedge clock)
+	begin
 	 if (control==1)					//Significa frecuencia
+		assign var=frecuencia;
+	else                          //Significa corriente
 		begin
-			assign auxiliar=frecuencia/10;
-			assign unidades=frecuencia-(auxiliar*10);
-			Memoria_display(
-				.CLK(clock),.numero(unidades), 
-				.controles_display(numero_cod));
-						
+		assign var=corriente;
 		end
 		
-	 else									//Significa corriente
-		begin
-		
-		end
-	 
-	 
-
-
+	selec_digito=0;
+	auxiliar<=var/10;
+	unidades<=var-(auxiliar*10);
+	num=unidades;
+   		
+	#20	
+	selec_digito<=1;
+	auxiliar<=var/100;
+	auxiliar2<=(var/10);
+	decenas<=auxiliar2-(auxiliar*10);
+	num=decenas;
+	#20
+	selec_digito<=2;
+	auxiliar<=frecuencia/1000;
+	auxiliar2<=(frecuencia/100);
+	centenas<=auxiliar2-(auxiliar*10);
+	num=centenas;
+	#20
+	selec_digito<=3;
+	auxiliar<=var/1000;
+	millares<=auxiliar;
+	num=millares;
+	end
+	Memoria_display(
+		.CLK(clock),.numero(num), 
+		.controles_display(numero_cod)
+		);	
 endmodule
